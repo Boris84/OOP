718036ddea5fb89aee965d1a0c0b54c7f66af806
Boris84 <simon.789@hotmail.co.uk>
2019-02-25 21:18:31 +0000

    update 1 p5 JavaScript

--- resources/js/sketch.js
+++ resources/js/sketch.js
@@ -1,7 +1,61 @@
-function setup() {
-  createCanvas(640, 480);
+
+
+let yPosList = [];
+let xPosList = [];
+let speedList = [];
+
+
+
+
+
+class Bubble {
+  constructor() {
+    this.xPos = Math.random() * 5990 + 1;
+    this.yPos = Math.random() * 600  + 1;
+    this.speed = Math.random() * 2 + 1;
+    this.r = Math.random() * 255;
+    this.g = Math.random() * 255;
+    this.b = Math.random() * 255;
+  }
+  
+   display() {
+     ellipse(this.xPos, this.yPos, 100, 100)
+     fill(this.r, this.g, this.b);
+   }
+
+   move() {
+  	 this.yPos += this.speed;
+  	 
+  	 if (this.yPos >= 660) {
+  	 	this.yPos = 0;
+  	 }
+   }
+
 }
 
-function draw() {
-  ellipse(50, 50, 80, 80);
+
+
+
+const bubbleList = [];
+  for (let i = 0; i < 100; i++) {
+  	bubbleList.push(new Bubble());
+  }
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
 }
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+ 
+function draw() {
+  background(255, 0, 255);
+  background("lightgreen");
+  for (var i = 0; i < 100; i++) {
+    bubbleList[i].display();
+    bubbleList[i].move();
+  }
+}
+  
+
